9532fedc486bdc0a8a20af140a4955d7ef344733
Gulcan Damdelen <gdamdelen@ccc.net>
2025-04-16 15:18:40 +0300

    Add cascade delete for files and folders

---
+++ prisma/migrations/20250416121801_add_cascade_delete/migration.sql
@@ -0,0 +1,17 @@
+-- DropForeignKey
+ALTER TABLE "files" DROP CONSTRAINT "files_folderId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "folders" DROP CONSTRAINT "folders_parentId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "shared_folders" DROP CONSTRAINT "shared_folders_folderId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "folders" ADD CONSTRAINT "folders_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "folders"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "files" ADD CONSTRAINT "files_folderId_fkey" FOREIGN KEY ("folderId") REFERENCES "folders"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "shared_folders" ADD CONSTRAINT "shared_folders_folderId_fkey" FOREIGN KEY ("folderId") REFERENCES "folders"("id") ON DELETE CASCADE ON UPDATE CASCADE;
